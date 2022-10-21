62ff1df915a9f1a1eaddeb4b5013efa63df35340
Sam123336 <115978232+Sam123336@users.noreply.github.com>
2022-10-21 08:55:39 +0530

    commiting
    a simple code for bank

---
+++ sam.py
@@ -0,0 +1,14 @@
+def deposit(a):
+  return a
+n=int(input("enter the amount")
+v1=deposit(n)
+print(v1,"has been credited")
+def withdraw(b):
+      return b
+N=int(input("enter the amount to be withdraw"))
+v2=withdraw(N)
+def account_detail(c):
+      print(diff,"amount left:")
+diff=n-N
+account_detail(diff)
+      
